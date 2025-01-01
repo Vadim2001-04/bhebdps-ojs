12902fe080a06308e8c5303d37bfdebb8ba7d64c
Vadim2001-04 <122891611+Vadim2001-04@users.noreply.github.com>
2025-01-01 12:46:05 +0300

    Update task.js

--- Домашние задания/2.2 Классы в JavaScript/task.js
+++ Домашние задания/2.2 Классы в JavaScript/task.js
@@ -92,3 +92,75 @@ picknick.state = 10;
 console.log(picknick.state); // 10
 picknick.fix();
 console.log(picknick.state); // 15
+
+
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        for (let book of this.books) {
+            if (book[type] === value) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    giveBookByName(bookName) {
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].name === bookName) {
+                const book = this.books[i];
+                this.books.splice(i, 1);
+                return book;
+            }
+        }
+        return null;
+    }
+}
+
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(
+    new DetectiveBook(
+        "Артур Конан Дойл",
+        "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+        2019,
+        1008
+    )
+);
+library.addBook(
+    new FantasticBook(
+        "Аркадий и Борис Стругацкие",
+        "Пикник на обочине",
+        1972,
+        168
+    )
+);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.findBookBy("name", "Властелин колец")); //null
+console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
+
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+const borrowedBook = library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
+
+borrowedBook.damage();
+console.log("Состояние книги после повреждения: " + borrowedBook.state); //Состояние книги после повреждения: 20
+
+borrowedBook.repair();
+console.log("Состояние книги после восстановления: " + borrowedBook.state); //Состояние книги после восстановления: 70
+
+library.addBook(borrowedBook);
+console.log("Количество книг после возвращения: " + library.books.length); //Количество книг после возвращения: 4
